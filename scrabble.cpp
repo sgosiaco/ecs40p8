3c98fd286ac35b663ba99546c80791d31f0ae48a
Sean Gosiaco <scgosiaco@ucdavis.edu>
2016-05-18 17:22:10 -0700

    comments galore and style changes

--- scrabble.cpp
+++ scrabble.cpp
@@ -20,61 +20,64 @@ int main(int argc, char** argv)
   set<string> words;
   string t = "", temp = "", s = "", orig = "";
 
-  for(int i = 0; i < 26; i ++)
+  for(int i = 0; i < 26; i ++) //assigning pt values to letters
     lettermap.insert(LetterMap::value_type(i + 'a', letterValues[i]));
 
-  ifstream inf("dictionary.txt");
-  while(getline(inf, temp))
-  {
-    transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
-    count = 0;
-    for(size_t j = 0; j < temp.length(); j++)
-      count += lettermap.at(temp.at(j));
-    wordmap.insert(WordMap::value_type(temp, count));
-  }
-  inf.close();
+  ifstream inf("dictionary.txt"); //open up file of valid words
 
-  ifstream in(argv[1]);
-  while(getline(in, s))
+  while(getline(inf, temp)) //read entire file
   {
-    orig = s;
-    sort(s.begin(), s.end());
+    transform(temp.begin(), temp.end(), temp.begin(), ::tolower); //lowercase
+    count = 0; //reset conut
+
+    for(size_t j = 0; j < temp.length(); j++) //entire string
+      count += lettermap.at(temp.at(j)); //add each char pt val to count
+    wordmap.insert(WordMap::value_type(temp, count)); //insert word + score
+  } //while
+  inf.close(); //close file
+
+  ifstream in(argv[1]); //open input
+  while(getline(in, s)) //read entire input
+  {
+    orig = s; //save string for print
+    //transform(s.begin(), s.end(), s.begin(), ::tolower); //lowercase
+    sort(s.begin(), s.end()); //sort to get all possible when using next_perm
+
     do
     {
-      for(size_t i = 2; i < s.length() + 1; i++) //2 s.length
+      for(size_t i = 2; i < s.length() + 1; i++) //get all substrings
       {
-        t = s.substr(0, i);
-        count = 0;
-        if(wordmap.find(t) != wordmap.end())
+        t = s.substr(0, i); //get substring
+        count = 0; //reset count
+        if(wordmap.find(t) != wordmap.end()) //word in map
         {
-          count = wordmap.at(t);
+          count = wordmap.at(t); //get count from map
           if(count >= best)
           {
             if(count > best)
             {
-              words.clear();
-              best = count;
+              words.clear(); //clear wordset
+              best = count; //set new best pts
             } //if
-            words.insert(t);
+            words.insert(t); //insert into worset
           } //if
         } //if
       }// for
-    } while(std::next_permutation(s.begin(), s.end()));
+    } while(std::next_permutation(s.begin(), s.end())); //give all perm
 
     cout << setw(2) << best << ' ' << orig << ": ";
-    if(!words.empty())
+
+    if(!words.empty()) //words
     {
       for(set<string>::iterator it = words.begin(); it != words.end(); it++)
          cout << *it << ' ';
       cout << endl;
-    }
-    else
-    {
+    } //if
+    else//no words
       cout << "No words found." << endl;
-    }
     words.clear();
     best = 0;
-  }
+  } //while
   in.close();
   return 0;
 }
