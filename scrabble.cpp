2d5efa791d6eee2591bc5ec1abf86c456090a670
sgosiaco <sgosiaco@me.com>
2016-05-22 19:45:49 -0700

    style changes

--- scrabble.cpp
+++ scrabble.cpp
@@ -10,18 +10,10 @@ using namespace std;
 typedef map<char, int> LetterMap;
 typedef map<string, int> WordMap;
 
-int main(int argc, char** argv)
+void setupDict(LetterMap &lettermap, WordMap &wordmap)
 {
-  int count = 0, best = 0, good = 1;
-  int letterValues[] = {1, 3, 3, 2, 1, 4, 2, 4, 2, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-  LetterMap lettermap;
-  WordMap wordmap;
-  set<string> words, blank;
-  string t = "", temp = "", s = "", orig = "";
-
-  for(int i = 0; i < 26; i ++) //assigning pt values to letters
-    lettermap.insert(LetterMap::value_type(i + 'a', letterValues[i]));
-
+  string temp;
+  int good = 1, count = 0;
   ifstream inf("words.txt"); //open up file of valid words
 
   while(getline(inf, temp)) //read entire file
@@ -44,7 +36,89 @@ int main(int argc, char** argv)
     } //if
   } //while
   inf.close(); //close file
+}
 
+void checkBlank(string &s, set<string> blank, int &good)
+{
+  if(s.length() > 0 && isspace(s.at(0))) //if blank
+  {
+    good = 0; //set flag for blank
+
+    for(int i = 0; i < 26; i++)
+    {
+      s.at(0) = 'a' + i;
+      blank.insert(s); //insert all 26 combinations
+    }
+  }
+  else
+  {
+    blank.insert(s);
+  }
+}
+
+void generateCombinations(set<string> blank, set<string> words, string &orig,
+string &s, WordMap wordmap, LetterMap lettermap, int &best, int &good)
+{
+  string t = "";
+  int count;
+
+  for(set<string>::iterator it = blank.begin(); it != blank.end(); it++)
+  {
+    s = *it;
+    sort(s.begin(), s.end()); //resort for max perm
+    do
+    {
+      for(size_t i = 2; i < s.length() + 1; i++) //get all substrings
+      {
+        t = s.substr(0, i); //get substring
+        count = 0; //reset count
+
+        if(wordmap.find(t) != wordmap.end()) //word in map
+        {
+          if(!good) //if blank remove blank pt score
+            count = wordmap.at(t) - lettermap.at((*it).at(0));
+          else //no blanks
+            count = wordmap.at(t); //get count from map
+
+          if(count >= best)
+            if(count > best)
+            {
+              words.clear(); //clear wordset
+              best = count; //set new best pts
+            } //if
+            words.insert(t); //insert into worset
+        } //if
+      }// for
+    } while(std::next_permutation(s.begin(), s.end())); //give all perm
+  }
+  cout << setw(2) << best << ' ' << orig << ":";
+}
+
+void printWords(set<string> &words)
+{
+  if(!words.empty()) //words
+  {
+    for(set<string>::iterator it = words.begin(); it != words.end(); it++)
+      cout << ' ' << *it;
+    cout << endl;
+  } //if
+  else//no words
+    cout << " No words found." << endl;
+}
+
+int main(int argc, char** argv)
+{
+  int best = 0, good = 1;
+  int letterValues[] = {1, 3, 3, 2, 1, 4, 2, 4, 2, 8, 5, 1, 3, 1, 1, 3, 10, 1,
+                        1, 1, 1, 4, 4, 8, 4, 10};
+  LetterMap lettermap;
+  WordMap wordmap;
+  set<string> words, blank;
+  string s = "", orig = "";
+
+  for(int i = 0; i < 26; i ++) //assigning pt values to letters
+    lettermap.insert(LetterMap::value_type(i + 'a', letterValues[i]));
+  setupDict(lettermap, wordmap);
   ifstream in(argv[1]); //open input
 
   while(getline(in, s)) //read entire input
@@ -53,61 +127,9 @@ int main(int argc, char** argv)
     orig = s; //save string for print
     sort(s.begin(), s.end()); //sort to get all possible when using next_perm
 
-    if(s.length() > 0 && isspace(s.at(0))) //if blank
-    {
-      good = 0; //set flag for blank
-
-      for(int i = 0; i < 26; i++)
-      {
-        s.at(0) = 'a' + i;
-        blank.insert(s); //insert all 26 combinations
-      }
-    }
-    else
-    {
-      blank.insert(s);
-    }
-
-    for(set<string>::iterator it = blank.begin(); it != blank.end(); it++)
-    {
-      s = *it;
-      sort(s.begin(), s.end()); //resort for max perm
-      do
-      {
-        for(size_t i = 2; i < s.length() + 1; i++) //get all substrings
-        {
-          t = s.substr(0, i); //get substring
-          count = 0; //reset count
-
-          if(wordmap.find(t) != wordmap.end()) //word in map
-          {
-            if(!good) //if blank remove blank pt score
-              count = wordmap.at(t) - lettermap.at((*it).at(0));
-            else //no blanks
-              count = wordmap.at(t); //get count from map
-            if(count >= best)
-            {
-              if(count > best)
-              {
-                words.clear(); //clear wordset
-                best = count; //set new best pts
-              } //if
-              words.insert(t); //insert into worset
-            } //if
-          } //if
-        }// for
-      } while(std::next_permutation(s.begin(), s.end())); //give all perm
-    }
-    cout << setw(2) << best << ' ' << orig << ":";
-
-    if(!words.empty()) //words
-    {
-      for(set<string>::iterator it = words.begin(); it != words.end(); it++)
-        cout << ' ' << *it;
-      cout << endl;
-    } //if
-    else//no words
-      cout << " No words found." << endl;
+    checkBlank(s, blank, good);
+    generateCombinations(blank, words, orig, s, wordmap, lettermap, best, good);
+    printWords(words);
 
     words.clear(); //clear output set
     blank.clear(); //clear input set
